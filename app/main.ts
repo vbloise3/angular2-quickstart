7af1d99110e669b7f41c1ece2532c318a39cf5e3
Vince Bloise <vbloise3@verizon.net>
2016-06-09 20:24:45 -0400

    Added main.ts

---
+++ app/main.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by vincebloise on 6/9/16.
+ */
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+bootstrap(AppComponent);
